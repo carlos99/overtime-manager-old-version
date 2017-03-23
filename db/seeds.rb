faeec86ae139f486be612bfdf11b5dba19faa1dc
Carlos Orellana <carlos@ufgcorp.com>
2017-03-22 20:57:57 -0600

    Added new seed for AdminUser. Also added edited post_dashboard in order to show status field

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,14 @@
 	Post.create!(date: Date.today, rationale: "#{post} rationale content")
 end
 
-puts "100 Posts have been created"
+puts "100 Posts has been created"
 
-# 100.times do |user|
-# 	User.create!(first_name: "Jon", last_name: "Snow", email: "jon#{user}.snow@gmail.com", password: "123456")
-# end
-#
-# puts "100 Users have been created"
+ 100.times do |user|
+ 	User.create!(first_name: "Jon", last_name: "Snow", email: "jon#{user}.snow@gmail.com", password: "123456")
+ end
+
+  puts "100 Users has been created"
+
+ AdminUser.create!(first_name: "Daenerys", last_name: "Targaryen", email: "admin@gmail.com", password: "123456")
+
+ puts "Admin User has been created"
