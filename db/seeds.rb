d5ef14cf185b54acf199dbbd735c88d7f2355240
Carlos Orellana <carlos@cw2106.@gmail.com>
2017-05-21 15:49:45 -0600

    Added phone number to the User model. Created notification.rake file. Separated test inside of spec/factories/users.rb and now won't be a problem adding new fields validations tu user. Added phone number to the dashboard for regular user and admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 100.times do |user|
-	@user = User.create(email: "jon#{user}.snow@gmail.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Snow")
+	@user = User.create(email: "jon#{user}.snow@gmail.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Snow", phone: "50321131856")
 end
 
 puts "100 Users Created"
 
-AdminUser.create(email: "admin@test.com", password: "123456", password_confirmation: "123456", first_name: "Admin", last_name: "Name")
+AdminUser.create(email: "admin@test.com", password: "123456", password_confirmation: "123456", first_name: "Admin", last_name: "Name", phone: "50321131856")
 
 puts "1 Admin User created"
 
