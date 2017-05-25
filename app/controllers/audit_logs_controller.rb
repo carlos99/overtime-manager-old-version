82f1e9d8074d3f5aef5a16673f6a0e808294c370
Carlos Orellana <carlos@cw2106.@gmail.com>
2017-05-24 23:47:25 -0600

    Added pagination to Audit Logs

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -2,7 +2,7 @@ class AuditLogsController < ApplicationController
 
   def index
     #The order is different like in post_controller because you need to instanciate the value that you are passing to the policy, in this case the @audit_logs
-    @audit_logs = AuditLog.all
+    @audit_logs = AuditLog.page(params[:page]).per(10)
     authorize @audit_logs
   end
 end
