f5386983bfabd31876dd5bc9977792afa4cb9956
Carlos Orellana <carlos@cw2106.@gmail.com>
2017-05-25 22:22:22 -0600

    Added basic styles to the homepage for admin. Now is possible to see the pending post from employees. Created partials and added more text to seeds files

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-    #code
+    @pending_approvals = Post.where(status: 'submitted')
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ AdminUser.create(email: "admin@test.com", password: "123456", password_confirmat
 puts "1 Admin User created"
 
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 3.5)
+	Post.create!(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", user_id: @user.id, overtime_request: 3.5)
 end
 
 puts "100 Posts have been created"
