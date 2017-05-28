2dc71cbc459353786bbbb21a3b3d76ef61f6161c
Carlos Orellana <carlos@cw2106.@gmail.com>
2017-05-28 16:14:06 -0600

    Added audit log to the homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
 
   def index
     @pending_approvals = Post.where(status: 'submitted').includes(:user)
+    @recent_audit_items = AuditLog.includes(:user).last(10)
   end
 end
