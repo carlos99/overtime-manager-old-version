8dcc49c81752a099e77225de966aaa2386377762
Carlos Orellana <carlos@cw2106.@gmail.com>
2017-05-27 23:24:11 -0600

    Added approve functionality from homepage. After hit approve, the post is approved. Added new policy in order to restric approval process just for admins. Also added the corresponding spec for approve

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-    @pending_approvals = Post.where(status: 'submitted')
+    @pending_approvals = Post.where(status: 'submitted').includes(:user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
 
   resources :audit_logs
+
   namespace :admin do
     resources :users
-resources :posts
-resources :admin_users
+    resources :posts
+    resources :admin_users
 
     root to: "users#index"
   end
 
-  resources :posts
+  resources :posts do
+    member do
+      get :approve
+    end
+  end
+
   devise_for :users, skip: [:registrations]
   root to: "welcome#index"
 end
