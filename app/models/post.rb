4045e76ca05c1fee57b3fd0982f57de64f5e1547
Carlos Orellana <carlos@cw2106.@gmail.com>
2017-06-06 21:42:38 -0600

    Added new callback in order to manage rejected post. Now if a request is rejected is showing up again.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,24 @@
-class Post < ApplicationRecord
-  enum status: { submitted: 0, approved: 1, rejected: 2 }
-  belongs_to :user
-  validates_presence_of :date, :rationale, :overtime_request
-  validates :overtime_request, numericality: {greater_than: 0.0}
+class Post < ActiveRecord::Base
+	enum status: { submitted: 0, approved: 1, rejected: 2 }
+	belongs_to :user
+	validates_presence_of :date, :rationale, :overtime_request
 
-  scope :posts_by, ->(user) { where(user_id: user.id) }
+	validates :overtime_request, numericality: { greater_than: 0.0 }
 
-  after_save :update_audit_log
+	scope :posts_by, ->(user) { where(user_id: user.id) }
 
-  private
+	after_save :confirm_audit_log, if: :submitted?
+	after_save :un_confirm_audit_log, if: :rejected?
 
-    def update_audit_log
-      audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
-      audit_log.confirmed! if audit_log
-    end
+	private
+
+		def confirm_audit_log
+			audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+			audit_log.confirmed! if audit_log
+		end
+
+		def un_confirm_audit_log
+			audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+			audit_log.pending! if audit_log
+		end
 end
